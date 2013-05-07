9e2bbc07726501dbcd41ac92318284f45e4a2202
wantedD <w4nt3d77@gmail.com>
2013-05-07 17:25:34 +0200

    Generatore Svg di Code39 (da xml)

--- Barcode_xml.py
+++ Barcode_xml.py
@@ -2,7 +2,7 @@ def Barcode(nome):
     from xml.dom.minidom import parse
     from simplySvg import createLine
     file = open(nome + '.svg', 'a+')
-    file.write('<?xml version="1.0" standalone="no"?>' + '\n' + '<svg width="'+ str(215) + '" height="'+ str(50) + '" version="1.1" xmlns="http://www.w3.org/2000/svg">' + '\n')
+    file.write('<?xml version="1.0" standalone="no"?>' + '\n' + '<svg width="'+ str(574) + '" height="'+ str(100) + '" version="1.1" xmlns="http://www.w3.org/2000/svg">' + '\n')
     parser = parse("/home/andrea/Scrivania/PyRe/BarCode-Python/data/Code39.xml") 
     carattere = parser.getElementsByTagName('carattere')
     startX = 0
@@ -14,13 +14,13 @@ def Barcode(nome):
            if lettera == name:
                for value in code:
                    if (value == "1"):
-                       elem = createLine(startX, 0, startX, 200, "black", 1)
+                       elem = createLine(startX, 0, startX, 100, "black", (574.0 / 216))
                    elif (value =="0"):
-                       elem = createLine(startX, 0, startX, 200, "white", 1)
+                       elem = createLine(startX, 0, startX, 100, "white", (574.0 / 216))
                    file.write(elem)
-                   startX += 1
+                   startX += (574.0 / 216)
                    print elem
-               spazio = createLine(startX, 0, startX, 200, "white", 1)
+               spazio = createLine(startX, 0, startX, 100, "white", (574.0 / 216))
                print spazio
                file.write(spazio)
     file.write('</svg>')
